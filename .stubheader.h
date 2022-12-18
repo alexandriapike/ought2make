d93a9e4814fbc0211c1054bed22bdb4ef59d7040
alexandria <alexandria@alexandria-linux.org>
2022-12-17 16:18:34 -0800

    .stubheader.h is a simple method for populating include/{libname}/{libname}.h

---
+++ .stubheader.h
@@ -0,0 +1,18 @@
+#ifndef OUGHT2MAKE_H
+#define OUGHT2MAKE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*	essential standard C library includes, data type declarations
+	and functon prototypes for an aought2make driven project go 
+	here i
+	replace OUGHT2MAKE_H with yourprojectname_H
+*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
